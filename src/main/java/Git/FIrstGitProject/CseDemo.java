99ada1cced1fc9481b82e4eefd8ff95b7613304a
sivapranaydakamari <sivapranay1423@gmail.com>
2026-02-14 15:08:50 +0530

    Added new Class methods

---
+++ src/main/java/Git/FIrstGitProject/CseDemo.java
@@ -0,0 +1,11 @@
+package Git.FIrstGitProject;
+
+public class CseDemo {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/test/java/Git/FIrstGitProject/CseDemoTest.java
@@ -0,0 +1,20 @@
+package Git.FIrstGitProject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CseDemoTest {
+
+	CseDemo cal = new CseDemo();
+	@Test
+	void test1(){
+		int result = cal.add(2, 3);
+		assertEquals(5, result);
+	}
+	@Test
+	void test2(){
+		int result = cal.mul(2, 3);
+		assertEquals(6, result);
+	}
+}
